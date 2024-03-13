d676b249c3ad062d11cf02e86f00ef890dbd2f39
Carlyle S. Davis <carlyle.davis@truewhitespace.com>
2024-03-13 19:30:42 -0400

    Referencing from the aws_record object.

--- lb.tf
+++ lb.tf
@@ -25,14 +25,14 @@ resource "aws_lb_target_group" "public" {
 }
 
 resource "aws_acm_certificate" "cert" {
-  domain_name       = aws_route53_record.public.dns_name
+  domain_name       = aws_route53_record.public.name
   validation_method = "DNS"
 
   tags = {
     Environment = "all"
   }
 
-  subject_alternative_names = [aws_route53_record.private.dns_name]
+  subject_alternative_names = [aws_route53_record.private.name]
 
   lifecycle {
     create_before_destroy = true
