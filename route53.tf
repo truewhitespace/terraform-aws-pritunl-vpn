ea28b0e7f16be50b372b3f98897e7f35b7043a3b
Carlyle S. Davis <carlyle.davis@truewhitespace.com>
2024-03-14 09:28:28 -0400

    Modifying how dns options work.

--- route53.tf
+++ route53.tf
@@ -4,7 +4,7 @@ data "aws_route53_zone" "this" {
 }
 
 resource "aws_route53_record" "public" {
-  count   = var.is_create_route53_reccord ? 1 : 0
+  # count   = var.is_create_route53_reccord ? 1 : 0
   zone_id = data.aws_route53_zone.this[0].zone_id
   name    = format("%s.%s", var.public_lb_vpn_domain, var.route53_zone_name)
   type    = "A"
