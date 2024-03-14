4545569d598f698381c331355903e3217c8b3ca0
Carlyle S. Davis <carlyle.davis@truewhitespace.com>
2024-03-14 09:40:19 -0400

    making sure routes get added in loop.

--- route53.tf
+++ route53.tf
@@ -5,14 +5,6 @@ data "aws_route53_zone" "this" {
 
 resource "aws_route53_record" "public" {
   # count   = var.is_create_route53_reccord ? 1 : 0
-  zone_id = data.aws_route53_zone.this[0].zone_id
-  name    = format("%s.%s", var.public_lb_vpn_domain, var.route53_zone_name)
-  type    = "A"
-  alias {
-    name                   = aws_lb.public.dns_name
-    zone_id                = aws_lb.public.zone_id
-    evaluate_target_health = true
-  }
 
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
@@ -21,6 +13,24 @@ resource "aws_route53_record" "public" {
       type   = dvo.resource_record_type
     }
   }
+
+  zone_id = data.aws_route53_zone.this[0].zone_id
+  # name    = format("%s.%s", var.public_lb_vpn_domain, var.route53_zone_name)
+  # type    = "A"
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+
+  alias {
+    name                   = aws_lb.public.dns_name
+    zone_id                = aws_lb.public.zone_id
+    evaluate_target_health = true
+  }
+
+
 }
 
 resource "aws_route53_record" "private" {
